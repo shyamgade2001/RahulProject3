806d9557b0dd885cc2a1d17c4dcff4be252fbf5b
Avnish <shyam_gade2001@yahoo.co.in>
2022-04-27 22:22:40 +0530

    This is from RahulProject 3 commit

--- src/Array_Calculations.java
+++ src/Array_Calculations.java
@@ -2,13 +2,14 @@ class Geekyshows
 {
 	public static void main(String args[])
 	{
-		int[] num= new int[5];
+		int[] num= new int[6];
 		num[0]=11;
 		num[1]=22;
 		num[2]=33;
 		num[3]=44;
 		num[4]=55;
-		for(int i=0; i<5; i++)
+		num[5]=66;
+		for(int i=0; i<6; i++)
 		{
 			System.out.println("num:-"+i+"+"+num[i]);
 			
