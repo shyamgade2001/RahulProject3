860dfcda5ae429148051db423f91c60ed407cccd
Avnish <shyam_gade2001@yahoo.co.in>
2022-04-27 21:38:13 +0530

    first commit

---
+++ src/Array_Calculations.java
@@ -0,0 +1,19 @@
+class Geekyshows
+{
+	public static void main(String args[])
+	{
+		int[] num= new int[5];
+		num[0]=11;
+		num[1]=22;
+		num[2]=33;
+		num[3]=44;
+		num[4]=55;
+		for(int i=0; i<5; i++)
+		{
+			System.out.println("num:-"+i+"+"+num[i]);
+			
+		}
+
+	
+	}
+}
---
+++ src/One_D_Array.java
@@ -0,0 +1,10 @@
+class Geekyshows1
+{
+	public static void main(String args[])
+	{
+	int num[]= new int[20];
+	int a;
+	a=num.length;
+	System.out.println(a);
+	}
+}
