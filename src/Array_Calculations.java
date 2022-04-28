00259939ac62483bb192405f20205b61e9976f0f
Avnish <shyam_gade2001@yahoo.co.in>
2022-04-28 17:11:21 +0530

    changed by rahulproject3

--- src/Array_Calculations.java
+++ src/Array_Calculations.java
@@ -2,15 +2,14 @@ class Geekyshows
 {
 	public static void main(String args[])
 	{
-		int[] num= new int[7];
+		int[] num= new int[5];
 		num[0]=11;
 		num[1]=22;
 		num[2]=33;
 		num[3]=44;
 		num[4]=55;
-		num[5]=66;
-		num[6]=77;
-		for(int i=0; i<7; i++)
+		
+		for(int i=0; i<5; i++)
 		{
 			System.out.println("num:-"+i+"+"+num[i]);
 			
